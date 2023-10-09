445416e66d5268af417b4cba9d68e59e13e85837
Achit Siddhu <114981698+asiddhu09@users.noreply.github.com>
2023-10-09 20:57:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//Dark mode
+const darkModeToggle = document.getElementById("dark-mode-toggle");
+const body = document.body;
+
+darkModeToggle.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+});
+//  Cards
